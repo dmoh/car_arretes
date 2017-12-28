f7aebbe8c0c03ea2a3bbd8e4aeb8f86189ba0cbc
@dmoh <mkanoute74@gmail.com>
2017-12-28 15:49:30 +0100

    Création de l'entité Car

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,20 +9,39 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use App\Entity\Cars;
 
 
 Class MainController extends Controller
 {
-    public function index()
+    public function index(Request $req)
     {
         $car = new Cars;
 
         $car->setImmat("BB-345-DD");
         $car->setMarque("Mercedes");
         $car->setProprio("Aps");
+        $car->setNumParc(9685);
+        $car->setSite("La Roche sur Foron");
+        $car->setNumSerie("WNNNNNNNSDSDN2323");
+        $car->setTypecar("Crossway");
+        $car->setTypePanne("C'est une panne dû au filtre a huile qui n'est pas très importante...");
 
-        
+
+
+
+
+        //recuperation de l'entity manager
+        $em = $this->getDoctrine()->getManager();
+
+        $em->persist($car);
+        $em->flush();
+
+
+
+
+
 
 
         $name = "Yags";
--- src/Entity/Cars.php
+++ src/Entity/Cars.php
@@ -8,20 +8,130 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Class Cars
+ * @package App\Entity
+ * @ORM\Entity
+ * @ORM\Table(name="cars")
+ */
 
 class Cars
 {
+    /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
     protected $id;
 
+    /**
+     * @ORM\Column(name="immat", type="string", length=100)
+     */
     protected $immat;
 
+    /**
+     * @ORM\Column(name="date", type="date")
+     */
     protected $date;
 
+
+    /**
+     * @ORM\Column(name="num_parc", type="integer")
+     */
+    protected $num_parc;
+
+    /**
+     * @ORM\Column(name="site", type="string", length=100)
+     */
+    protected $site;
+
+    /**
+     * @ORM\Column(name="num_serie", type="string", length=100)
+     */
+    protected $num_serie;
+
+    /**
+     * @ORM\Column(name="typecar", type="string", length=100)
+     */
+    protected $typecar;
+
+    /**
+     * @ORM\Column(name="marque", type="string", length=100)
+     */
     protected $marque;
 
-    protected $proprio;
+    /**
+     * @ORM\Column(name="locataire", type="string", length=20)
+     */
+    protected $locataire;
+
+    /**
+     * @ORM\Column(name="type_panne", type="text")
+     */
+    protected $type_panne;
+
+    /**
+     * @ORM\Column(name="date_prev", type="date", nullable=true)
+     */
+    protected $date_prev;
+
+    /**
+     * @ORM\Column(name="date_mar", type="date", nullable=true)
+     */
+    protected $date_mar;
+
+    /**
+     * @ORM\Column(name="deb_garantie", type="string", length=100, nullable=true)
+     */
+    protected $deb_garantie;
+
+    /**
+     * @ORM\Column(name="fin_garantie", type="date", nullable=true)
+     */
+    protected $fin_garantie;
+
+    /**
+     * @ORM\Column(name="action_engages", type="text", nullable=true)
+     */
+    protected $action_engages;
+
+    /**
+     * @ORM\Column(name="etat_car", type="string", length=100)
+     */
+    protected $etat_car;
+
+    /**
+     * @ORM\Column(name="date_ev", type="date", nullable=true)
+     */
+    protected $date_ev;
+
+    /**
+     * @ORM\Column(name="date_mec", type="date", nullable=true)
+     */
+    protected $date_mec;
 
 
+    /**
+     * @ORM\Column(name="duree_garantie", type="string", nullable=true)
+     *
+     */
+    protected $duree_garantie;
+
+
+    /**
+     * @ORM\Column(name="proprio_pneus", type="string", nullable=true)
+     */
+    protected $proprio_pneus;
+
+
+
+
+    public function __construct()
+    {
+        $this->date = new \Datetime();
+    }
 
 
     public function setId($id)
@@ -29,9 +139,12 @@ class Cars
         $this->id = $id;
     }
 
+    /**
+     * @return mixed
+     */
     public function getId()
     {
-        return $this->id = $id;
+        return $this->id;
     }
 
     public function setMarque($marque)
@@ -64,19 +177,19 @@ class Cars
     }
 
     /**
-     * @param mixed $proprio
+     * @param mixed $locataire
      */
-    public function setProprio($proprio): void
+    public function setLocataire($locataire): void
     {
-        $this->proprio = $proprio;
+        $this->locataire = $locataire;
     }
 
     /**
      * @return mixed
      */
-    public function getProprio()
+    public function getLocataire()
     {
-        return $this->proprio;
+        return $this->locataire;
     }
 
     /**
@@ -94,6 +207,245 @@ class Cars
     public function getDate()
     {
         return $this->date;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNumParc()
+    {
+        return $this->num_parc;
+    }
+
+    /**
+     * @param mixed $num_parc
+     */
+    public function setNumParc($num_parc): void
+    {
+        $this->num_parc = $num_parc;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNumSerie()
+    {
+        return $this->num_serie;
+    }
+
+    /**
+     * @param mixed $num_serie
+     */
+    public function setNumSerie($num_serie): void
+    {
+        $this->num_serie = $num_serie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSite()
+    {
+        return $this->site;
+    }
+
+    /**
+     * @param mixed $site
+     */
+    public function setSite($site): void
+    {
+        $this->site = $site;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTypecar()
+    {
+        return $this->typecar;
+    }
+
+    /**
+     * @param mixed $type
+     */
+    public function setTypecar($typecar): void
+    {
+        $this->typecar = $typecar;
+    }
+
+    public function getTypePanne()
+    {
+        return $this->type_panne;
+    }
+
+    /**
+     * @param mixed $type_panne
+     */
+    public function setTypePanne($type_panne): void
+    {
+        $this->type_panne = $type_panne;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDatePrev()
+    {
+        return $this->date_prev;
+    }
+
+    /**
+     * @param mixed $date_prev
+     */
+    public function setDatePrev($date_prev): void
+    {
+        $this->date_prev = $date_prev;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateMar()
+    {
+        return $this->date_mar;
+    }
+
+    /**
+     * @param mixed $date_mar
+     */
+    public function setDateMar($date_mar): void
+    {
+        $this->date_mar = $date_mar;
+    }
+
+
+    /**
+     * @param mixed $deb_garantie
+     */
+    public function setDebGarantie($deb_garantie): void
+    {
+        $this->deb_garantie = $deb_garantie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDebGarantie()
+    {
+        return $this->deb_garantie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getFinGarantie()
+    {
+        return $this->fin_garantie;
+    }
+
+    /**
+     * @param mixed $fin_garantie
+     */
+    public function setFinGarantie($fin_garantie): void
+    {
+        $this->fin_garantie = $fin_garantie;
+    }
+
+    /**
+     * @param mixed $action_engages
+     */
+    public function setActionEngages($action_engages): void
+    {
+        $this->action_engages = $action_engages;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getActionEngages()
+    {
+        return $this->action_engages;
+    }
+
+    /**
+     * @param mixed $etat_car
+     */
+    public function setEtatCar($etat_car): void
+    {
+        $this->etat_car = $etat_car;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEtatCar()
+    {
+        return $this->etat_car;
+    }
+
+
+    /**
+     * @return mixed
+     */
+    public function getDateEv()
+    {
+        return $this->date_ev;
+    }
+
+    /**
+     * @param mixed $date_ev
+     */
+    public function setDateEv($date_ev): void
+    {
+        $this->date_ev = $date_ev;
+    }
+
+    /**
+     * @param mixed $date_mec
+     */
+    public function setDateMec($date_mec): void
+    {
+        $this->date_mec = $date_mec;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDateMec()
+    {
+        return $this->date_mec;
+    }
+
+    /**
+     * @param mixed $duree_garantie
+     */
+    public function setDureeGarantie($duree_garantie): void
+    {
+        $this->duree_garantie = $duree_garantie;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDureeGarantie()
+    {
+        return $this->duree_garantie;
+    }
+
+    /**
+     * @param mixed $proprio_pneus
+     */
+    public function setProprioPneus($proprio_pneus): void
+    {
+        $this->proprio_pneus = $proprio_pneus;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getProprioPneus()
+    {
+        return $this->proprio_pneus;
     }
 
 
